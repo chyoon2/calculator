cf14bd6246201b78994f1fdd36a08fcb0d1551a7
Chris Yoon <chy.yoon@gmail.com>
2020-06-01 11:26:28 -0700

    add operator functions to scripts.js
    Co-authored-by: Jeff Kim <kim2jy@hotmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,20 @@
-const add = function(number1, number2) {
-	return number1 + number2;
-};
-
+// User interface logic
 const number1 = parseInt(prompt("Enter a number:"));
 const number2 = parseInt(prompt("Enter another number:"));
 const result = add(number1, number2);
 alert(result);
+
+// Business logic
+
+const add = function(number1, number2) {
+	return number1 + number2;
+};
+const subtract = function(number1, number2) {
+  return number1 - number2;
+};
+const multiply = function(number1, number2) {
+  return number1 * number2;
+};
+const divide = function(number1, number2) {
+  return number1 / number2;
+};
